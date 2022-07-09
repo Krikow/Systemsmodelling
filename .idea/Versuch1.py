674bb2fa262ada5ae8bf4c9ed28d0e2174fe9575
justus <Justus.L1@web.de>
2022-07-09 20:32:34 +0200

    Läuft

--- .idea/Versuch1.py
+++ .idea/Versuch1.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
+
 df_with_duplicates = pd.read_excel("/Users/justus/Desktop/Studium/Systemmodellierung/ZugeschnitteneDaten1.xlsx")
 
 df_without_duplicates = df_with_duplicates.drop_duplicates(subset=['Status description','Status date/time'])
 
-df_without_duplicates.to_excel("Ergebnis_Datenaufbereitung1.xlsx")
+#df_without_duplicates.to_excel("Ergebnis_Datenaufbereitung1.xlsx")
 
 id_list = np.unique(df_without_duplicates["Shipment no."])
 
@@ -17,7 +18,7 @@ for id in id_list:
         continue
 
     else:
-        print(False, id)
+        #print(False, id)
         df_only_shipmentcreation.drop(df_only_shipmentcreation[df_only_shipmentcreation['Shipment no.'] == id].index, inplace=True)
 
 id_list2 = np.unique(df_only_shipmentcreation["Shipment no."])
@@ -29,15 +30,24 @@ for id in id_list2:
         continue
 
     else:
-        print(False, id)
+        #print(False, id)
         df_only_shipmentcreation.drop(df_only_shipmentcreation[df_only_shipmentcreation['Shipment no.'] == id].index, inplace=True)
 
+df_only_shipmentcreation = df_only_shipmentcreation.reset_index()
 
-#for status in statuslist:
-    #if df_without_duplicates[df_without_duplicates["Status description"] s.str.startswith('Not out for delivery yet')]
-    #status.rename
+for idx in range(len(df_only_shipmentcreation["Status description"])):
+    #print(idx)
+    if "Not out for delivery yet" in df_only_shipmentcreation["Status description"][idx]:
+        #print(idx, df_only_shipmentcreation["Status description"][idx])
+        df_only_shipmentcreation["Status description"][idx] = "Not out for delivery yet"
 
+    else:
+        #print(False)
+        continue
+
+#print(df_only_shipmentcreation["Status description"].value_counts())
 
 df_only_shipmentcreation.to_excel("Humbug.xlsx")
+
 print("Länge df_only_shipmentcreation:", len(df_only_shipmentcreation),",", "Länge df_without_duplicates:", len(df_without_duplicates))
 
